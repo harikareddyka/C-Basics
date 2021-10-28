8297b31103c6051a49f946a7d624dd7459b3a537
harikareddyka <92074857+harikareddyka@users.noreply.github.com>
2021-10-28 09:43:05 +0530

    Add files via upload

---
+++ problem.c
@@ -0,0 +1,19 @@
+/*additing two point values*/
+#include<stdio.h>
+void main()
+{
+	
+	//variable declaratio
+	float number1, number2, sum;
+	
+	//initialization
+	number1 = 9.5;
+	number2 = 9.7;
+	
+	//calculation
+	sum = number1+number2; //9.5+9.7 = 19.2
+	
+	//output
+	printf("sum of %f and %f is %f",number1,number2,sum);
+	
+}
