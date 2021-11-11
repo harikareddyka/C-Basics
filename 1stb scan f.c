cf38665273689271e6736eb54cedcf6009f5f7bc
harikareddyka <92074857+harikareddyka@users.noreply.github.com>
2021-11-11 10:46:17 +0530

    Add files via upload

---
+++ 1stb scan f.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+void main() 
+{ 
+     int h,a,r,k;
+     printf("enter four numbers");
+     scanf("%d%d%d",&h,&a,&r,&k);
+     /*printf("Enter 1st number:");
+     scanf("%d",&h);
+     printf("Enter 2nd number:");
+     scanf("%d",&a);
+     printf("Enter 3rd number:");
+     scanf("%d",&r);
+     printf("enter 4th number:");
+     scanf("%d",&k)
+     printf("%d",h*a*r*k);*/
+     
+}
+	  
+    
+        
+
---
+++ area and perimeter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	 int side,area,perimeter;
+	 printf("enter the side length");
+	 scanf("%d",&side);
+	 area = side*side;
+	 perimeter = 4*side;
+	 printf("area of square is %d\n",area);
+	 printf("perimeter of square is %d",perimeter);
+	 
+}
