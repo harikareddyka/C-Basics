4fe9a85cc1e4bd3ffd50e22aebc2afac39453cf4
harikareddyka <92074857+harikareddyka@users.noreply.github.com>
2021-11-12 10:00:50 +0530

    Add files via upload

---
+++ 2 scan f.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	 int minutes;
+	 printf("enter the minutes");
+	 scanf("%d",&minutes);
+	 printf("the sum of minutes is %d hours \n",minutes/60);
+	 printf("minutes is %d",minutes%60);
+}
