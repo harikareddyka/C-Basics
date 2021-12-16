71c0c8feb1d9f214fee12c1249198ecbc90ac103
harikareddyka <92074857+harikareddyka@users.noreply.github.com>
2021-12-16 09:44:22 +0530

    Add files via upload

---
+++ decision making.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main ()
+  {
+    int number;
+    printf("enter your number");
+    scanf("%d" , &number);
+    if (number>0)
+    { 
+       printf("the given number is positive");
+	}
+else
+	{ 
+	   printf("the given number is negative");
+	}
+  } 
+
+    
+   
